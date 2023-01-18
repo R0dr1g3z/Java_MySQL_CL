823c8abf50dd37ab938347fabb4ab98c6e7fad21
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-18 15:13:16 +0100

    format doc

--- src/main/java/com/example/javamysql/Main12.java
+++ src/main/java/com/example/javamysql/Main12.java
@@ -9,19 +9,19 @@ public class Main12 {
     public static void main(String[] args) throws SQLException {
         Scanner scan = new Scanner(System.in);
         String query = "SELECT * FROM cinemas ";
-        try(PreparedStatement preparedStatement = DbUtil.connect("cinemas_ex2").prepareStatement(query)){
+        try (PreparedStatement preparedStatement = DbUtil.connect("cinemas_ex2").prepareStatement(query)) {
             ResultSet resultSet = preparedStatement.executeQuery();
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 System.out.println("Id: " + resultSet.getInt("cinema_id") + " Name: " + resultSet.getString("name"));
             }
         }
         System.out.println("Pick cinema id:");
         int id = scan.nextInt();
         String query2 = "SELECT * FROM cinemas JOIN movies WHERE cinema_id = ?";
-        try(PreparedStatement preparedStatement = DbUtil.connect("cinemas_ex2").prepareStatement(query2)){
+        try (PreparedStatement preparedStatement = DbUtil.connect("cinemas_ex2").prepareStatement(query2)) {
             preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 System.out.println(resultSet.getString("title"));
             }
         }
